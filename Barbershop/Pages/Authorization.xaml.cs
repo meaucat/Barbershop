5262c70ca4d2a975ed00f745a6d3cefd905dc876
meaucat <yuuummm55@gmail.com>
2023-09-28 22:20:00 +0300

    Добавьте файлы проекта.

---
+++ Barbershop/Pages/Authorization.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Barbershop.DB;
+
+namespace Barbershop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        public static List<Employee> employees { get; set; }
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void SwitchRegBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("Pages/Registration.xaml", UriKind.Relative));
+        }
+
+        private void AuthActionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text.Trim();
+            string password = PasswordBox.Password.Trim();
+
+            employees = new List<Employee>(DBConnection.barberShopEnt.Employee.ToList());
+            Employee currentUser = employees.FirstOrDefault(x => x.Login == login && x.Password == password);
+            if (currentUser != null)
+                NavigationService.Navigate(new Main("Pages/Main.xaml", UriKind.Relative));
+            else
+                MessageBox.Show("Что-то введено неверно. Попробуйте снова");
+        }
+    }
+}
---
+++ Barbershop/Pages/Registration.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Barbershop.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Barbershop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Page
+    {
+        public static List<Employee> employees { get; set; }
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void RegButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text.Trim();
+            string password = PasswordBox.Password.Trim();
+            string name = NameTextBox.Text.Trim();
+            string surname = SurnameTextBox.Text.Trim();
+            string patronymic = PatronymicTextBox.Text.Trim();
+
+            Employee employee = new Employee();
+            employee.Login = login;
+            employee.Password = password;
+            employee.FirstName = name;
+            employee.LastName = surname;
+            employee.Patronymic = patronymic;
+            DBConnection.barberShopEnt.Employee.Add(employee);
+            DBConnection.barberShopEnt.SaveChanges();
+
+            employees = new List<Employee>(DBConnection.barberShopEnt.Employee.ToList());
+            Employee currentUser = employees.FirstOrDefault(x => x.Login == login && x.Password == password);
+
+            NavigationService.Navigate(new Main(currentUser));
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("Pages/Authorization.xaml", UriKind.Relative));
+        }
+    }
+}
